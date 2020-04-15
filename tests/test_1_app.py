bb5a3c2e4c915c2ccc7261b16ec4d84c26aa270e
frmdstryr <frmdstryr@protonmail.com>
2020-04-15 13:59:25 -0400

    Re-enable app test

--- tests/test_1_app.py
+++ tests/test_1_app.py
@@ -4,7 +4,7 @@ import signal
 import pytest
 import subprocess
 
-@pytest.mark.skipif('TRAVIS' in os.environ, reason='Disabled for now')
+
 def test_app():
     p = subprocess.Popen(
         'declaracad', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
